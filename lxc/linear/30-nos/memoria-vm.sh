fc626b42f50bda9bf490122a4e0322edc425d53c
Antonio Junior <antonio.jr004@gmail.com>
2018-04-10 14:57:00 -0300

    cpu e memoria

---
+++ lxc/linear/30-nos/memoria-vm.sh
@@ -0,0 +1,15 @@
+# /bin/bash
+i=0
+tempo=300
+
+intervalo=1073741824
+
+
+while [ $i -lt $tempo ]
+do
+	val=`free -hm`	
+	echo $val # >>"memoria-VM-linear-30".ods
+
+	sleep 2
+	i=`expr $i + 1`
+done
